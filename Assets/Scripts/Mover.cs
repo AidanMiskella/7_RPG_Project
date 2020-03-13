8a97db6f8ff2c135ae9d856378ecbdd42985a053
Aidan Miskella <aidanmiskella10@gmail.com>
2020-03-13 16:37:57 +0000

    player imports and basic movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -12,10 +12,12 @@ public class Mover : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButton(0)) {
 
             MoveToCursor();
         }
+
+        UpdateAnimator();
     }
 
     private void MoveToCursor() {
@@ -28,5 +30,13 @@ public class Mover : MonoBehaviour {
 
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator() {
+
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
